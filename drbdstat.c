a01b09fd6663ab08b7febb97fe1699a8adb9361f
root <root@colodisk02.gie.com>
2016-02-25 13:59:13 -0600

    Update for new glibc

--- drbdstat.c
+++ drbdstat.c
@@ -1,6 +1,6 @@
-/* drbdstat 1.6
+/* drbdstat 1.7
  * Simple program to continuously display DRBD status with adjustable update speed
- * (c)2012-2013 Miles Lott <mlott@gie.com>
+ * (c)2012-2016 Miles Lott <mlott@gie.com>
  *
  * Compile with: gcc -o drbdstat drbdstat.c
  *
@@ -17,12 +17,15 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * $Id: drbdstat.c,v 1.6 2013/11/27 13:26:18 gulfconfig Exp $
+ * $Id: drbdstat.c,v 1.6 2016/02/25 19:52:27 gulfconfig Exp $
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main(void)
 {
@@ -31,6 +34,7 @@ int main(void)
 	char ref[15];
 	char state[15];
 	char buf[1000];
+	struct stat sbuf;
 	char HOSTNAME[4096];
 	int delay = 1;
 	int detail = 0;
@@ -38,7 +42,15 @@ int main(void)
 	gethostname(HOSTNAME, 4096);
 
 	/* Fetch DRBD version */
-	ptr_version = fopen("/sys/module/drbd/version","r");
+	if(stat("/sys/module/drbd/version",&sbuf) > -1)
+	{
+		ptr_version = fopen("/sys/module/drbd/version","r");
+	}
+	else
+	{
+		fprintf(stderr,"Unable to open DRBD version file.  Is DRBD running?\n");
+		exit(0);
+	}
 	while(fgets(ver,16, ptr_version)!=NULL)
 	{
 		printf("%s",ver);
@@ -50,7 +62,7 @@ int main(void)
 	while(1)
 	{
 		/* Fetch initstate, if available */
-		if(stat("/sys/module/drbd/initstate") > -1)
+		if(stat("/sys/module/drbd/initstate",&sbuf) > -1)
 		{
 			ptr_state = fopen("/sys/module/drbd/initstate","r");
 			while(fgets(state,16, ptr_state)!=NULL)
@@ -63,6 +75,8 @@ int main(void)
 		else
 		{
 			strncpy(state,"unknown",7);
+			fprintf(stderr,"Unable to open DRBD initstate file.  Is DRBD running?\n");
+			exit(0);
 		}
 
 		/* Fetch refcnt */
@@ -81,11 +95,7 @@ int main(void)
 		}
 
 		/* Read and print the status file */
-		if(stat("/proc/drbd"))
-		{
-			ptr_file = fopen("/proc/drbd","r");
-		}
-		else
+		if(!(ptr_file = fopen("/proc/drbd","r")))
 		{
 			fprintf(stderr,"Unable to open DRBD process file.  Is DRBD running?\n");
 			exit(0);
